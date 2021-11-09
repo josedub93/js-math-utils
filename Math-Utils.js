c9cde74667250806f2e0fb433805db3de341c7f6
Josedub <josedub93@gmail.com>
2021-11-09 19:54:34 -0300

    primer push

--- Math-Utils.js
+++ Math-Utils.js
@@ -1,5 +1,16 @@
+function suma(a, b){
+    console.log(a + b)
+    return a + b
+}
+
 function resta(a, b){
     console.log(a - b)
     return a - b
 }
-resta(13,5)
+function multiplicacion(a, b){
+    console.log(a * b)
+    return a * b
+}
+suma(2+2);
+resta(8-3);
+multiplicacion(13,5);
