207d04889635c1fbe7c43a4e695b66e5da8db982
Josedub <josedub93@gmail.com>
2021-11-09 19:59:55 -0300

    prueba push

--- Math-Utils.js
+++ Math-Utils.js
@@ -11,6 +11,12 @@ function multiplicacion(a, b){
     console.log(a * b)
     return a * b
 }
+
+function division(a, b){
+    console.log(a / b)
+    return a / b
+}
 suma(2+2);
 resta(8-3);
 multiplicacion(13,5);
+division(8.2);
